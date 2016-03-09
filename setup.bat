6b5e8b2c4376f8311162dcf8feaccb726c3a94ed
unknown <2109381e@BO715-11-01.ad.dcs.gla.ac.uk>
2016-03-09 16:59:56 +0000

    Intial commit

---
+++ setup.bat
@@ -0,0 +1,8 @@
+easy_install pip
+pip install virtualenv
+pip install virtualenvwrapper-win
+mkvirtualenv megabite
+workon megabite
+pip install django-registration-redux
+pip install -r requirements.txt
+pause
