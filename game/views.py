24dffe4e8be3af549fbfa2a2356f1dba8ebac055
sosaje <jonathansaje@Jonathans-MacBook-Pro.local>
2016-03-18 22:52:18 +0000

    Register Page, Slight changes to base

--- game/views.py
+++ game/views.py
@@ -11,3 +11,15 @@ def index(request):
     # Note that the first parameter is the template we wish to use.
 
     return render(request, 'index.html', context_dict)
+    
+def register(request):
+
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': "I am bold font from the context"}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+
+    return render(request, 'register.html', context_dict)
