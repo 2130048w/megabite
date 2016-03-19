972ea7edb894f58941362f6b5d315ae96d681390
AislingMcMahon <amcmaho6@tcd.ie>
2016-03-19 19:14:15 +0000

    Changes to registration page

--- megabite/urls.py
+++ megabite/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from registration.backends.simple.views import RegistrationView
 
+class MyRegistrationView(RegistrationView):
+    def get_success_url(request, user):
+        return '/game/'
+		
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'tango_with_django_project_17.views.home', name='home'),
@@ -8,4 +13,6 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^game/', include('game.urls')), # ADD THIS NEW TUPLE!
+	url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
+	url(r'^accounts/', include ('registration.backends.simple.urls')),
 )
