4017f7bfd187721245cc384c1595b37f9647da32
AislingMcMahon <amcmaho6@tcd.ie>
2016-03-21 04:06:02 +0000

    Game in views, simple GUI

--- game/views.py
+++ game/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from game.models import Player
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core import exceptions
+import zgame
 
 def index(request):
     return render(request, 'index.html', {})
@@ -15,3 +18,38 @@ def leaderboards(request):
     
 def intro(request):
 	return render(request, 'intro.html')
+
+def game(request):
+	contextDict = {}
+	try:
+		u = request.user
+	except exceptions.ObjectDoesNotExist:
+		return HttpResponseRedirect('/accounts/register')
+	g = zgame.Game()
+	g.start_new_day()
+	if not g.is_game_over():
+        #kick off the day
+		g.start_new_day()
+        if not g.is_day_over() and not g.is_game_over():
+			contextDict['options'] = g.turn_options
+			contextDict['state'] = str(zgame.PlayerState())
+			contextDict['gstate'] = g.game_state
+			if request.method == 'POST':
+				if 'MOVE' in request.POST:
+					g.take_turn('MOVE')
+				if 'ENTER' in request.POST:
+					g.take_turn('ENTER')
+				if 'WAIT' in request.POST:
+					g.take_turn('WAIT')	
+				if 'FIGHT' in request.POST:
+					g.take_turn('FIGHT')
+				if 'RUN' in request.POST:
+					g.take_turn('RUN')
+				if 'EXIT' in request.POST:
+					g.take_turn('EXIT')	
+				if 'SEARCH' in request.POST:
+					g.take_turn('SEARCH')	
+        # end the day
+        g.end_day()
+        #u.game = pickle(g)
+	return render(request, 'game.html', contextDict)
