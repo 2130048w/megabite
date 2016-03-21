1f575c0bf6930e670073cdc3b95c9124b482f0f7
AislingMcMahon <amcmaho6@tcd.ie>
2016-03-21 12:59:39 +0000

    Edit Profile page

--- game/urls.py
+++ game/urls.py
@@ -5,10 +5,12 @@ urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
         url(r'^register/$', views.register, name='register'),
         url(r'^login/$', views.myLogin, name='myLogin'),
-	url(r'^leaderboards/$', views.leaderboards, name='leaderboards'),
+		url(r'^leaderboards/$', views.leaderboards, name='leaderboards'),
         url(r'^intro/$', views.intro, name='intro'),
         url(r'^play/$', views.game, name='game'),
-        url(r'^safehouse/$', views.safehouse, name='safehouse'))
+        url(r'^safehouse/$', views.safehouse, name='safehouse'),
+		url(r'^edit_profile/$', views.edit_profile, name='edit_profile'))
+		
 
 
 
